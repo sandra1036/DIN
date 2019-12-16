0d8d9e05d920165609c00542cafb6cb3afac44a0
sandra1036 <sandraromero262@gmail.com>
2019-12-16 13:55:26 +0100

    Añadido Styles

---
+++ Excercices/Styles/StylesOp.py
@@ -0,0 +1,37 @@
+from tkinter import ttk
+from tkinter import *
+
+window = Tk()
+frame=ttk.Frame(window)
+frame.pack()
+s=ttk.Style()
+s.configure('TButton', font='helvetica 24',background="white")
+s.configure('TLabel',background="red",foreground="blue")
+s.configure('TRadiobutton',background="orange",foreground="green")
+s.configure('TCheckbutton',background="black",foreground="white")
+s.configure('TCombobox',background="purple",foreground="white")
+label=ttk.Label(frame,text="Label")
+label.pack()
+button=ttk.Button(frame,text="Button")
+button.pack()
+radio=ttk.Radiobutton(frame,text="Radio")
+radio.pack()
+check=ttk.Checkbutton(frame,text="Check")
+check.pack()
+ChVar=StringVar()
+sel=ttk.Combobox(frame,textvariable=ChVar)
+sel.pack()
+sel["values"]=s.theme_names()
+sel.bind("<<ComboboxSelected>>",lambda e: s.theme_use(ChVar.get()))
+
+
+
+
+
+
+
+
+window=mainloop()
+
+
+
