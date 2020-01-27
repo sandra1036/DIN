84f2c104ca2d5af47cca5326f7c998a72f0c0fc9
sandra1036 <sandraromero262@gmail.com>
2020-01-27 13:50:33 +0100

    Creado ya las flechas

--- Excercices/Telesketch/telesketchM.py
+++ Excercices/Telesketch/telesketchM.py
@@ -10,22 +10,33 @@ def xy(event):
     global lastx, lasty
     lastx, lasty = event.x, event.y
 
+def punto():
+    global lastx,lasty
+    canvasD.cr
+
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
 canvasD=Canvas(window,relief=RAISED,scrollregion=(0, 0, 1000, 1000))
-canvasD.grid(column=0,row=0,sticky=(N,W,S,E))
+canvasD.pack()
 canvasD.configure(background="blue")
 
 canvasC=Canvas(window,relief=RAISED)
-canvasC.grid(column=0,row=2,sticky=(W, E))
+canvasC.pack()
 canvasC.configure(background="red")
-points = [0, 25, 50,25, 25, 0]
+points = [325, 0, 300,25, 350, 25]
 uparrow=canvasC.create_polygon(points,fill="black")
 
-points2=[0, 25, 25,25, 25, 50]
-leftarrow=canvasC.create_polygon(points2,fill="black")
+points2 = [325, 55, 300,30, 350, 30]
+downarrow=canvasC.create_polygon(points2,fill="black")
+
+points3=[0, 25, 25,0, 25, 50]
+leftarrow=canvasC.create_polygon(points3,fill="black")
+
+points4=[55, 25, 30,0, 30, 50]
+rightarrow=canvasC.create_polygon(points4,fill="black")
+
 canvasC.bind("<Button-1>", lambda e:print(e.x,e.y))
 window.update()
-window.maxsize(600,500)
-window.minsize(400,500)
+window.maxsize(500,340)
+window.minsize(300,340)
 window.mainloop()
