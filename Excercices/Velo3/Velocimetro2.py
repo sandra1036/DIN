b229dd56dc9f2ca6124b7b5ff9594283cb3f93ba
sandra <sandraromero262@gmail.com>
2020-02-10 13:17:34 +0100

    Solucion Velocimetro

---
+++ Excercices/Velo3/Velocimetro2.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter import ttk
+import numpy as np
+from Speedodom import Speedodom
+import threading
+import time
+
+def worker():
+    b = 240
+    while True:
+        if b == 0:
+            b,l,st = 240,0,-2
+        else:
+            b,l,st = 0,240,10
+        for v in np.arange(b, l, st):
+            time.sleep(0.5)
+            spd.setspeed(v)
+    return
+root = Tk()
+f = ttk.Frame()
+f.pack()
+spd = Speedodom(f, width=200, height=400)
+spd.grid(row=0, column=0)
+t = threading.Thread(target=worker).start()
+root.mainloop()
+
+
+
